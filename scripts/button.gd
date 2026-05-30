3b6ebaed302373a7f0382e6635b2d7367d012723
Mike Harris <jammin804@gmail.com>
2026-05-30 19:26:20 -0400

    working on main menu and settings

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,10 +1,13 @@
 extends Button
 
 @export var click: AudioStreamPlayer
+@export var hover: AudioStreamPlayer
 
 func _ready() -> void:
 	text = text.to_upper()
-	print(text.to_upper())
 
 func _on_pressed() -> void:
 	click.play() 
+
+func _on_mouse_entered() -> void:
+	hover.play()
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,15 @@
 class_name MainMenu
 extends Control
 
+func _ready() -> void:
+	%StartBtn.pressed.connect(play)
+
+func play():
+	get_tree().change_scene_to_file("res://scenes/lab.tscn")
 
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_btn_pressed() -> void:
+	%SettingsPanelContainer.show()
