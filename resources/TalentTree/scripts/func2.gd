247e68913523696404d26a8fcd1df0226c07f909
Mike Harris <jammin804@gmail.com>
2026-08-13 21:47:45 -0400

    Update func2.gd
    Fixed the health upgrade loop when the do the lab exploit

--- resources/TalentTree/scripts/func2.gd
+++ resources/TalentTree/scripts/func2.gd
@@ -1,11 +1,16 @@
 extends TalentFunctionality
 class_name HealthChange
 
+var is_health_applied: bool = false
+
 func action(talentIcon: TalentIcon):
 	var health = talentIcon.talent_resource.stat_value
 
-	PlayerManager.current_stats.battery_tank_points += health
-	print("New MAX HP: ", PlayerManager.current_stats.battery_tank_points)
+	if is_health_applied == false:
+		PlayerManager.current_stats.battery_tank_points += health
+		#print("New MAX HP: ", PlayerManager.current_stats.battery_tank_points)
+		is_health_applied = true
+
 	talentIcon.talent_resource.is_unlocked = true
 
 	Events.max_health_upgraded.emit(PlayerManager.current_stats.battery_tank_points)
