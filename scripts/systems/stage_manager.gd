be5a6a78dce91b22129400ebda8c104d56adcf14
Mike Harris <jammin804@gmail.com>
2026-08-13 20:55:40 -0400

    Added diamond scene tranistion for polish

--- scripts/systems/stage_manager.gd
+++ scripts/systems/stage_manager.gd
@@ -1,34 +1,38 @@
 class_name StageManager
 extends CanvasLayer
 
-@export var levels : Array[String] 
+signal scene_loading_finished
+
+@export var levels : Array[String]
 
 var scene_to_load : String
 var active_level : Node2D
 
 
 @onready var transition_player: AnimationPlayer = $TransitionPlayer
-
+#TODO Fix timing for music to start and stop after level loads
 func _ready() -> void:
-	#print(levels.size())
+	visible = false
 	pass
 
 func change_scene_to(scene_path: String) -> void:
+	visible = true
 	scene_to_load = scene_path
-	transition_player.play("fade_out")
+	transition_player.play("transition")
 	get_tree().paused = true
+	await transition_player.animation_finished
+	_load_new_scene()
 
 func _load_new_scene() -> void:
 	get_tree().paused = false
+	transition_player.play_backwards("transition")
 	get_tree().call_deferred("change_scene_to_file", scene_to_load)
-	transition_player.play("fade_in")
+	await transition_player.animation_finished
+	visible = false
+	scene_loading_finished.emit()
 
 
-func _on_transition_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "fade_out":
-		_load_new_scene()
-		
-func _on_change_level() -> void:
+func _on_change_level() -> void: #FIXME Remove. Not being used
 	match Globals.level:
 		1:
 			print("Go to level 1")
