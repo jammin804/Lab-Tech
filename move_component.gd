f2c34f416008d25d4b55ed0b7f1647eda1a0660c
Mike Harris <jammin804@gmail.com>
2026-08-19 17:33:31 -0400

    Added up down movement to player to move to different lanes

--- move_component.gd
+++ move_component.gd
@@ -1,7 +1,18 @@
 extends Node
 
+@export var player : PlayerNew
+
+var lane_distance : float = 60
+
+func _ready() -> void:
+	print("Current Player Position ", player.global_position)
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_up"):
+		player.global_position += Vector2.ZERO - Vector2(0, lane_distance)
+		print(player.global_position)
 		print("move up a row")
 	if Input.is_action_just_pressed("move_down"):
+		player.global_position += Vector2.ZERO + Vector2(0, lane_distance)
+		print(player.global_position)
 		print("move down a row")
