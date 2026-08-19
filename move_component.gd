d6f207b5ae34f0f065a684e90ec485415fe57d6e
Mike Harris <jammin804@gmail.com>
2026-08-19 17:38:11 -0400

    Removed some print statments

--- move_component.gd
+++ move_component.gd
@@ -4,15 +4,9 @@ extends Node
 
 var lane_distance : float = 60
 
-func _ready() -> void:
-	print("Current Player Position ", player.global_position)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_up"):
 		player.global_position += Vector2.ZERO - Vector2(0, lane_distance)
-		print(player.global_position)
-		print("move up a row")
 	if Input.is_action_just_pressed("move_down"):
 		player.global_position += Vector2.ZERO + Vector2(0, lane_distance)
-		print(player.global_position)
-		print("move down a row")
