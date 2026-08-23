c00b0528df23051012f8eb4c930cd20f76bd1cd3
Mike Harris <jammin804@gmail.com>
2026-08-22 21:26:35 -0400

    Added new font and made new themes for settings

--- scripts/ui/video_settings.gd
+++ scripts/ui/video_settings.gd
@@ -15,20 +15,21 @@ func _ready() -> void:
 	]
 	for res in resolutions:
 		resolution_option.add_item("%dx%d" % [res.x, res.y] )
-	
+
 	#load_current_settings()
-	
+
 	resolution_option.item_selected.connect(_on_resolution_select)
 	fullscreen_check.toggled.connect(_on_fullscreen_toggled)
 	borderless_check.toggled.connect(_on_borderless_toggled)
 	vsync_check.toggled.connect(_on_vsync_toggled)
-	
+
+
 func load_current_settings() -> void: #TODO: Need to remove in the future. Currently scared it will break the game
 	var mode = DisplayServer.window_get_mode()
 	fullscreen_check.button_pressed = mode == DisplayServer.WINDOW_MODE_FULLSCREEN
 	borderless_check.button_pressed = DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_BORDERLESS)
 	vsync_check.button_pressed = DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED
-	
+
 	var window_size = DisplayServer.window_get_size()
 	for i in range(resolution_option.item_count):
 		var res_text = resolution_option.get_item_text(i)
@@ -36,14 +37,14 @@ func load_current_settings() -> void: #TODO: Need to remove in the future. Curre
 		if parts.size() == 2 and int(parts[0]) == window_size.x and int(parts[1]) == window_size.y:
 			resolution_option.select(i)
 			break
-			
-			
+
+
 func _on_resolution_select(index: int):
 	var text = resolution_option.get_item_text(index)
 	var parts = text.split("x")
 	if parts.size() == 2:
 		DisplayServer.window_set_size(Vector2i(int(parts[0]), int(parts[1])))
-		
+
 func _on_fullscreen_toggled(enabled: bool) -> void:
 	if enabled:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
@@ -52,8 +53,8 @@ func _on_fullscreen_toggled(enabled: bool) -> void:
 
 func _on_borderless_toggled(enabled: bool):
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, enabled)
-	
-	
+
+
 func _on_vsync_toggled(enable: bool):
 	var mode = DisplayServer.VSYNC_ENABLED if enable else DisplayServer.VSYNC_DISABLED
 	DisplayServer.window_set_vsync_mode(mode)
