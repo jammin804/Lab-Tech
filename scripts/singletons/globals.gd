d39e6e3d77a8035a9d0179f69eba8c8eecc79fde
Mike Harris <jammin804@gmail.com>
2026-09-25 13:25:50 -0400

    Updated the Global Level to 1 to allow the enemy default to be greater than 0 (#18)

--- scripts/singletons/globals.gd
+++ scripts/singletons/globals.gd
@@ -3,7 +3,7 @@ class_name Global
 
 var current_wave: int = 1
 var moving_to_next_wave: bool
-var level: int = 0
+var level: int = 1
 var total_levels : int = 3
 var can_screenshake : bool = true #When damage or upgrade is at a certian level screen shake
 var enemies_killed : int = 0
